339aefb952ab56e4d7d4666058e7275fd64997b7
Onkar Kubal <onkar82kubal@gmail.com>
2021-08-24 01:30:09 +0530

    Middleware added

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -9,7 +9,7 @@ const Product = ({product}) => {
             <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant='top' />                
             </Link>
-            <Card.Body>
+            <Card.Body>               
             <Link to={`/product/${product._id}`}>
                 <Card.Title as='div'>
                     <strong>{product.name}</strong>
